69e3609c43582ab9c29901cb154a89bb0e8069e6
Kartik Nath <kartiknath@gmail.com>
2022-01-08 18:35:32 -0500

    My first commit for example JS app

---
+++ main.js
@@ -0,0 +1,55 @@
+//alert('Kartik Nath - 2022');
+//console.log('log note #1');
+//console.error('error writing to file');
+//console.warn('this is only a warning');
+
+
+//variables
+//var let and const 
+// var was the original variable but we don't want to use it any more
+
+const owner = 'Kartik Nath';
+const age = 43;
+//let age = 30;
+//age = 31;
+
+console.log('This page is maintained by', owner,'-', 'His age is '+ age);
+
+//datatypes
+//primitive = directly assigned to memory
+//strings numbers boolean null undefined Symbol 
+
+const isAlive = true;
+const rating = 4.5; //no float or decimal datatype in Javascript
+const x = null; 
+const y = undefined;
+
+console.log (typeof y);
+
+//objects for example arrays
+
+// Let's talk about strings 
+// Concatenation 
+//Template String
+const hello = `My name is ${owner} and I am ${age}`;
+console.log(hello);
+
+const s = 'Hello World';
+s.toUpperCase();
+
+console.log(s);
+console.log(s.toUpperCase());
+console.log(s.split(''));
+//console.log(s.length);
+
+
+
+
+
+
+
+
+
+
+
+
